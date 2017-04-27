4c49cce0228a5ac0043746dfd81c48131fccf4f5
Mahesh <mahesh.muddasani90@gmail.com>
2017-04-27 22:38:18 +0530

    linkedlist

--- AlgoAndDataStructures/src/com/ds/linkedlistproblems/LinkedListNode.java
+++ AlgoAndDataStructures/src/com/ds/linkedlistproblems/LinkedListNode.java
@@ -24,5 +24,41 @@ public class LinkedListNode {
 	public void setNext(LinkedListNode next) {
 		this.next = next;
 	}
+	
+	public int size(LinkedListNode headNode){
+		int length=0;
+		LinkedListNode currentNode = headNode;
+		while(currentNode != null){
+			length++;
+			currentNode = currentNode.getNext();
+		}
+		return length;
+	}
 
+	public LinkedListNode insertInLinkedList(LinkedListNode headNode, LinkedListNode nodeToInsert, int position){
+		if(headNode == null)
+			return nodeToInsert;
+		
+		int size= size(headNode);
+		if(position>size+1 || position<1){
+			System.out.println("Position of node to insert is invalid. The valid inputs are 1 to "+(size+1));
+			return headNode;
+		}
+		if(position==1){
+			nodeToInsert.setNext(headNode);
+			return nodeToInsert;
+		}else{
+			LinkedListNode previousNode = headNode;
+			int count= 1;
+			while(count<position-1){
+				previousNode = previousNode.getNext();
+				count++;
+			}
+			
+			LinkedListNode currentNode = previousNode.getNext();
+			nodeToInsert.setNext(currentNode);
+			previousNode.setNext(nodeToInsert);
+		}
+		return headNode;
+	}
 }
