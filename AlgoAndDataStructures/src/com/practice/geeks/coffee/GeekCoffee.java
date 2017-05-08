115ad4565e12e59df1e61d6516813d71309563f9
muddm <mahesh.muddasani90@gmail.com>
2017-05-08 14:49:54 +0530

    dfdf

---
+++ AlgoAndDataStructures/src/com/practice/geeks/coffee/GeekCoffee.java
@@ -0,0 +1,27 @@
+package com.practice.geeks.coffee;
+
+import java.util.Scanner;
+
+public class GeekCoffee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int testcases = s.nextInt();
+		while(testcases>0){
+			int initialunits = s.nextInt();
+			int refillCount = s.nextInt();
+			System.out.println(calculateUnitsOfCoffe(initialunits,refillCount,1));
+			testcases--;
+		}
+	}
+
+	private static int calculateUnitsOfCoffe(int initialunits, int refillCount, int countunits) {
+		// TODO Auto-generated method stub
+		if(countunits==refillCount){
+			return initialunits;
+		}
+		return calculateUnitsOfCoffe(initialunits/2, refillCount,++countunits);
+	}
+
+}
